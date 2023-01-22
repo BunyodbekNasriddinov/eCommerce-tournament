f75954c7e991b1faa3ced460a4a4cd2ecf9ee794
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-01-22 19:47:53 +0500

    Add testimonials

--- js/slick/main.js
+++ js/slick/main.js
@@ -1,6 +1,13 @@
-$('.product__list').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+$(".product__list").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+$(".testimonial__wrapper").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
